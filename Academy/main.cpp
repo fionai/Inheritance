9d36ae2a7e1a574f4b521a15a4d9e57c85fe96d1
Ada <Ada@PROBOOK>
2025-07-19 18:13:14 +0700

    polymorphism

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -4,6 +4,8 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#define delimiter "\n---------------------\n"
+
 #define HUMAN_TAKE_PARAMETERS const std::string& last_name, const std::string& first_name, int age
 #define HUMAN_GIVE_PARAMETERS last_name, first_name, age
 
@@ -46,12 +48,12 @@ public:
 		set_age(age);
 		cout << "HConstructor:\t" << this << endl;
 	}
-	~Human()
+	virtual ~Human()
 	{
 		cout << "HDestructor:\t" << this << endl;
 	}
 	//	Methods
-	void info() const
+	virtual void info() const
 	{
 		cout << last_name << " " << first_name << " " << age << endl;
 	}
@@ -117,7 +119,7 @@ public:
 		cout << "SDestructor:\t" << this << endl;
 	}
 	//	Methods
-	void info() const
+	void info() const override
 	{
 		Human::info();
 		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
@@ -165,7 +167,7 @@ public:
 
 	}
 	// Methods
-	void info() const
+	void info() const override
 	{
 
 		Human::info();
@@ -190,16 +192,21 @@ public:
 	{
 		cout << "GDestructor:\t" << this << endl;
 	}
-		void info()const
+		void info()const override
 		{ 
 			Student::info();
 			cout << subject << endl;
 		}
 };
 
+//#define INHERITANCE
+#define POLYMORPHISM
+
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef INHERITANCE
 	Human human("Montana", "Antonio", 25);
 	human.info();
 
@@ -211,4 +218,24 @@ void main()
 
 	Graduate graduate("Schreder", "Hank", 40, "Criminalistic", "WW_220", 40, 60, "How to catch Heisenberg");
 	graduate.info();
+#endif // INHERITANCE
+
+	Human* group[] =
+	{
+		new Human("Montana", "Antonio", 25),
+		new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 99),
+		new Teacher("White", "Walter", 50, "Chemistry", 25),
+		new Graduate("Schreder", "Hank", 40, "Criminalistic", "WW_220", 40, 60, "How to catch Heisenberg"),
+		new Student("Vercetty", "Tommy", 30, "Theft", "Vice", 98, 99),
+		new Teacher("Diaz", "Ricardo", 50, "Weapons disrtibution", 20)
+	};
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		group[i]->info();
+		cout << delimiter;
+	}
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		delete group[i];
+	}
 }
