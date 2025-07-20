5ecce86c3c84bb96b698dd203a771a3d544ce834
Ada <Ada@PROBOOK>
2025-07-20 17:23:10 +0700

    cout class, write group and read group DONE

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -1,5 +1,7 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include <string>
+#include <stdio.h>
 #include <fstream>
 
 using std::cin;
@@ -71,7 +73,7 @@ public:
 	{
 		std::fstream fout;
 		fout.open(FIleName, std::fstream::app);
-		fout << endl << last_name << " " << first_name << " " << age << " ";
+		fout << endl << last_name << "|" << first_name << "|" << age << "|";
 		fout.close();
 	}
 	/*std::ostream& operator<< (std::ostream& os)
@@ -159,7 +161,7 @@ public:
 		Human::write_to_file(FIleName);
 		std::fstream foutS;
 		foutS.open(FIleName, std::fstream::app);
-		foutS << speciality << " " << group << " " << rating << " " << attendance << " ";
+		foutS << speciality << "|" << group << "|" << rating << "|" << attendance << "|";
 		foutS.close();
 	}
 }; //Student
@@ -226,7 +228,7 @@ public:
 		Human::write_to_file(FileName);
 		std::ofstream foutT;
 		foutT.open(FileName, std::fstream::app);
-		foutT << speciality << " " << experience << " ";
+		foutT << speciality << "|" << experience << "|";
 		foutT.close();
 	}
 };  //Teacher
@@ -274,7 +276,7 @@ public:
 			Student::write_to_file(FileName);
 			std::ofstream foutG;
 			foutG.open(FileName, std::fstream::app);
-			foutG << subject << " ";
+			foutG << subject << "|";
 			foutG.close();
 		}
 };  //Graduate
@@ -287,7 +289,8 @@ std::ostream& operator<< (std::ostream& os, const Graduate& obj)
 //#define INHERITANCE
 //#define POLYMORPHISM
 //#define STD_COUT
-#define WRITE_GROUP_TO_FILE
+//#define WRITE_GROUP_TO_FILE
+//#define TEST_READ_GROUP_FROM_FILE
 #define READ_GROUP_FROM_FILE
 
 void main()
@@ -343,22 +346,12 @@ void main()
 	cout << "Graduate: " << graduate << endl;
 #endif // STD_COUT
 
-#ifdef WRITE_GROUP_TO_FILE
 	string FileName = "GroupFile.txt";
+#ifdef WRITE_GROUP_TO_FILE
 	std::ofstream fout;
 	fout.open(FileName);	//открываем файл на ПЕРЕзапись
 	fout << "Our group:";	//очищаем файл в начале каждого запуска программы
 	fout.close();
-
-	/*Human human("Montana", "Antonio", 25);
-	human.write_to_file(FileName);
-	Student student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 99);
-	student.write_to_file(FileName);
-	Teacher teacher("White", "Walter", 50, "Chemistry", 25);
-	teacher.write_to_file(FileName);
-	Graduate graduate("Schreder", "Hank", 40, "Criminalistic", "WW_220", 40, 60, "How to catch Heisenberg");
-	graduate.write_to_file(FileName);*/
-
 
 	Human* group[] =
 	{
@@ -382,14 +375,161 @@ void main()
 	//string sys_command;
 	//sys_command = "notepad " + FileName + "\0";
 	//cout << sys_command;
-	system("notepad GroupFile.txt");	//нельзя передать имя файла из переменной??
+	
+#ifdef DEBUG
+	system("notepad GroupFile.txt");	//нельзя передать имя файла из переменной??  
+#endif // DEBUG
+
 
 #endif // WRITE_GROUP_TO_FILE
-	//Human* groupTEST[6];
-	//groupTEST[0] = new Human("Montana", "Antonio", 25);
+
+#ifdef TEST_READ_GROUP_FROM_FILE
+	//============TEST============
+	int n = 6;
+	Human** groupTEST = new Human * [n];
+	groupTEST[0] = new Human("", "", 0);
+	groupTEST[0]->set_first_name("Montana");
+	groupTEST[0]->set_last_name("Antonio");
+	groupTEST[0]->set_age(25);
+	cout << *groupTEST[0];
+	//============TEST============  
+#endif // TEST_READ_GROUP_FROM_FILE
 
 
 #ifdef READ_GROUP_FROM_FILE
+	std::ifstream fin(FileName);
+
+	int n = 0;
+	if (fin.is_open())
+	{
+		//просто считаем строки. Количество строк-1 - это количество элементов в группе
+		while (!fin.eof())
+		{
+			const int SIZE = 1024;
+			char line_buf[SIZE] = {};
+			fin.getline(line_buf, SIZE);
+#ifdef DEBUG
+			cout << n << ": " << line_buf << endl;
+#endif // DEBUG
+
+			n++;
+		}
+		n--;
+		fin.close();
+	} //посчитали строки
+	else 	{	std::cerr << "Error: file not found.\n";	}
+	if (!n) 	{	cout << "0 group members is found in file \"" << FileName << "\"\n";	}
+	else if (n > 0)     //если есть строки, предположительно с элементами классов, 
+						// снова открываем файл, создаем group и тд
+	{
+		Human** group_from_file = new Human* [n];
+		std::ifstream fin(FileName);
+		if (fin.is_open())
+		{
+			int n_current = 0;  //текущий элемент строки (порядковый номер)
+			const int SIZE = 1024;
+			char str[SIZE] = {};
+			fin.getline(str, SIZE); // первую строку считываем впустую, она нам не нужна
+			while (!fin.eof())
+			{
+				for (int i = 0; i < n; i++)
+				{
+					//char str[SIZE] = {};
+					char* pch;
+					//int current_int = 0;
+					fin.getline(str, SIZE);
+					// сохраняем копию строки
+					char str_buf[SIZE] = {};
+					int j = 0;
+					do
+					{
+						str_buf[j] = str[j];
+						j++;
+					} while (str[j]);
+					//ENd OF сохраняем копию строки
+
+					//анализируем и обрабатываем строки:
+
+					int n_elements = 0; // количество элементов в строке
+					pch = strtok(str, "|");
+					while (pch)
+					{
+#ifdef DEBUG
+						cout << pch << " ";
+#endif // DEBUG
+
+						pch = strtok(NULL, "|");
+						n_elements++;
+					}  //просто посчитали количество переменных в строке и ничего и ними не делали
+#ifdef DEBUG
+					cout << " - " << n_elements << endl;
+#endif // DEBUG
+
+					char* ln = strtok(str_buf, "|");
+					char* fn = strtok(NULL, "|");
+					int age_tmp = atoi(strtok(NULL, "|"));
+					switch (n_elements) //в зависимости от количества элементов создаем нужный класс в группе
+					{
+					case 3: {
+						group_from_file[n_current] = new Human(ln, fn, age_tmp);
+						break;
+					}
+					case 5: {
+						//char* ln = strtok(str_buf, "|");
+						//char* fn = strtok(NULL, "|");
+						//int age_tmp = atoi(strtok(NULL, "|"));
+						char* sp = strtok(NULL, "|");
+						int exp = atoi(strtok(NULL, "|"));
+						group_from_file[n_current] = new Teacher(ln, fn, age_tmp, sp, exp);
+						break;
+					}
+					case 7: {
+						//char* ln = strtok(str_buf, "|");
+						//char* fn = strtok(NULL, "|");
+						//int age_tmp = atoi(strtok(NULL, "|"));
+						char* sp = strtok(NULL, "|");
+						char* gr = strtok(NULL, "|");
+						int rate = atoi(strtok(NULL, "|"));
+						int att = atoi(strtok(NULL, "|"));
+						group_from_file[n_current] = new Student(ln, fn, age_tmp, sp, gr, rate, att);
+						break;
+					}
+					case 8: {
+						//char* ln = strtok(str_buf, "|");
+						//char* fn = strtok(NULL, "|");
+						//int age_tmp = atoi(strtok(NULL, "|"));
+						char* sp = strtok(NULL, "|");
+						char* gr = strtok(NULL, "|");
+						int rate = atoi(strtok(NULL, "|"));
+						int att = atoi(strtok(NULL, "|"));
+						char* subj = strtok(NULL, "|");
+						group_from_file[n_current] = new Graduate(ln, fn, age_tmp, sp, gr, rate, att, subj);
+						break;
+					}
+					}
+					//END OF анализируем и обрабатываем строки.
+
+					n_current++;
+				}  //END OF for
+			}  //while (!fin.eof())
+			fin.close();
+
+			//=======================DELETE!!!==============================
+			for (int i = 0; i < n; i++)  //sizeof(group_from_file) / sizeof(group_from_file[0]) - не работает!!!
+			{
+				cout << delimiter;
+				group_from_file[i]->info();
+				//cout << *group_from_file[i] << endl;
+			}
+			for (int i = 0; i < n; i++)
+			{
+				delete group_from_file[i];
+			}
+			delete[] group_from_file;
+
+		}  //if (fin.is_open())
+		else { std::cerr << "Error: file not found.\n"; }
+	}
 
 #endif // READ_GROUP_FROM_FILE
 
